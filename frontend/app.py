52fbf4a73cc8ac4d8bac6060a23104846a2a17ce
Yogasree-21122006 <Yogasree2112@gmail.com>
2026-03-10 20:49:56 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import requests
 from PIL import Image
 
-# Backend API URL
-API_URL = "https://facescope-api.onrender.com/verify"
+API_URL = "http://127.0.0.1:8000/verify"
 
 st.title("Face Recognition System")
 st.write("Upload a CROPPED face image for verification.")
@@ -15,38 +14,26 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file:
 
-    # Show uploaded image
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Prepare file for API
     files = {
-        "file": (uploaded_file.name, uploaded_file.getvalue(), "image/jpeg")
+        "file": uploaded_file.getvalue()
     }
 
-    try:
-        # Send request to backend
-        response = requests.post(API_URL, files=files, timeout=30)
+    response = requests.post(API_URL, files=files)
 
-        if response.status_code == 200:
-            data = response.json()
+    if response.status_code == 200:
+        data = response.json()
 
-            st.write(f"### Best Match: {data['best_match']}")
-            st.write(f"### Similarity Score: {data['similarity_score']:.4f}")
+        st.write(f"### Best Match: {data['best_match']}")
+        st.write(f"### Similarity Score: {data['similarity_score']:.4f}")
 
-            if data["result"] == "VERIFIED":
-                st.success("VERIFIED")
-            else:
-                st.error("UNKNOWN PERSON")
-
+        if data["result"] == "VERIFIED":
+            st.success("VERIFIED")
         else:
-            st.error("API Error: Unable to get response")
+            st.error("UNKNOWN PERSON")
 
-    except requests.exceptions.ConnectionError:
-        st.error("Cannot connect to backend API")
+    else:
+        st.error("API Error")
 
-    except requests.exceptions.Timeout:
-        st.error("Request timed out. Try again.")
-
-    except Exception as e:
-        st.error(f"Unexpected error: {e}")
